a0ce5421796e62006890597fbc9f89e545504d28
rotairo <85470341+rotairo@users.noreply.github.com>
2021-06-10 16:46:24 +0800

    Add files via upload

---
+++ arithmetic.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Arithmetic Operators</title>
+</head>
+<body>
+
+	<?php
+	$a = 42;
+	$b = 20;
+
+	$c = $a + $b;
+	echo "Addition Operation Result: $c <br/>";
+	$c = $a - $b;
+	echo "Substraction Operation Result: $c <br/>";
+	$c = $a * $b;
+	echo "Multiplicationn Operation Result: $c <br/>";
+	$c = $a / $b;
+	echo "Division Operation Result: $c <br/>";
+	$c = $a % $b;
+	echo "Modulus Operation Result: $c <br/>";
+	$c = $a ++;
+	echo "Increment Operation Result: $c <br/>";
+	$c = $a --;
+	echo "Decrement Operation Result: $c <br/>";
+
+</body>
+</html>
